50867c66f8fd6b6e7c23d80d30a7206b9fdca917
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-06-08 17:13:00 +0300

    Updated light theme.

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -2,30 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class LightTheme {
-  final _lightColors = _LightColors();
+  final lightColors = LightColors();
 
   late ThemeData theme; // debug modda isen late final kullanmamalısın degisiklikleri görmek için final kaldır
 
   LightTheme() {
     theme = ThemeData(
-      scaffoldBackgroundColor: _lightColors._white.withOpacity(0.9),
-      appBarTheme: const AppBarTheme(
-        centerTitle: true,
-        elevation: 0,
-        systemOverlayStyle: SystemUiOverlayStyle.light,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(16))),
-      ),
-    );
+        scaffoldBackgroundColor: LightColors.white,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(16))),
+        ));
   }
 }
 
-class _LightColors {
-  final Color _white = const Color.fromARGB(255, 250, 250, 250);
-  final Color _seaGreen = const Color.fromARGB(255, 0, 161, 157);
-  final Color _deepSeaBlue = const Color.fromARGB(255, 0, 38, 66);
-  final Color _oceanBlue = const Color.fromARGB(255, 1, 58, 99);
-  final Color _pureWhite = const Color.fromARGB(255, 255, 255, 255);
-  final Color _turquoiseBlue = const Color.fromARGB(255, 2, 128, 144);
-  final Color _romeoRed = const Color.fromARGB(255, 167, 36, 60);
-  final Color _grappleGreen = const Color.fromARGB(255, 64, 184, 177);
+class LightColors {
+  static const Color white = Color.fromARGB(255, 250, 250, 250);
+  static const Color seaGreen = Color.fromARGB(255, 0, 161, 157);
+  static const Color deepSeaBlue = Color.fromARGB(255, 0, 38, 66);
+  static const Color oceanBlue = Color.fromARGB(255, 1, 58, 99);
+  static const Color pureWhite = Color.fromARGB(255, 255, 255, 255);
+  static const Color turquoiseBlue = Color.fromARGB(255, 2, 128, 144);
+  static const Color romeoRed = Color.fromARGB(255, 167, 36, 60);
+  static const Color grappleGreen = Color.fromARGB(255, 64, 184, 177);
+  static const Color steelyGrey = Color.fromARGB(255, 141, 154, 154);
+  static const Color finishFiordGreen = Color.fromARGB(255, 89, 177, 188);
 }
