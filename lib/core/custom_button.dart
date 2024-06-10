e02e4fb57b3e1c0ad92939f9d3f8c063e856eceb
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-06-11 01:57:21 +0300

    Updated custom button.

--- lib/core/custom_button.dart
+++ lib/core/custom_button.dart
@@ -2,10 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:mobway_clone_app/theme/light_theme.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text, required this.onPressed});
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.backgroundColor = LightColors.finishFiordGreen,
+    this.textColor = LightColors.pureWhite,
+    this.fontSize = 16,
+  });
 
   final String text;
   final VoidCallback onPressed;
+  final Color backgroundColor;
+  final Color textColor;
+  final double fontSize;
 
   @override
   Widget build(BuildContext context) {
@@ -14,20 +24,14 @@ class CustomButton extends StatelessWidget {
       child: ElevatedButton(
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-          backgroundColor: LightColors.finishFiordGreen,
+          backgroundColor: backgroundColor,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
           padding: const EdgeInsets.symmetric(vertical: 15),
-          textStyle: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
+          textStyle: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold),
         ),
-        child: Text(
-          text,
-          style: const TextStyle(color: LightColors.white),
-        ),
+        child: Text(text, style: TextStyle(color: textColor)),
       ),
     );
   }
